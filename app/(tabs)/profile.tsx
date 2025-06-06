d852e0951edc46ea163e117e1f111bcf208293f5
ManishAvadhiya <avadhiyamanish@gmail.com>
2025-06-06 13:17:30 +0530

    ovies added

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const profile = () => {
+  return (
+    <View className="h-full flex-col items-center justify-center">
+      <Text className="text-5xl text-primary mb-4">My name is Manish.</Text>
+      
+    </View>
+  )
+}
+
+export default profile
